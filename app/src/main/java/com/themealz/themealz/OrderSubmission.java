201c61d2f12aab13be443f06b931a8d5020cdfb0
Eli Sagy <sagyeli@gmail.com>
2015-12-07 14:57:18 +0200

    Raw structure of order submission activity

--- app/src/main/java/com/themealz/themealz/OrderSubmission.java
+++ app/src/main/java/com/themealz/themealz/OrderSubmission.java
@@ -1,14 +1,26 @@
 package com.themealz.themealz;
 
-import android.os.Bundle;
 import android.app.Activity;
+import android.graphics.Typeface;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class OrderSubmission extends Activity {
+
+    private TextView mMainTitle;
+    private Button mSubmitButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_order_submission);
+
+        mMainTitle = (TextView) findViewById(R.id.main_title);
+        mSubmitButton = (Button) findViewById(R.id.submit_button);
+
+        mMainTitle.setTypeface(Typeface.createFromAsset(getAssets(), "fonts/regular.ttf"));
+        mSubmitButton.setTypeface(Typeface.createFromAsset(getAssets(), "fonts/regular.ttf"));
     }
 
 }
