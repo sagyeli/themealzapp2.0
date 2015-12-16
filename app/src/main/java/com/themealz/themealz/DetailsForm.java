528787f04a05a3fa8811ed39765716314a3d0edb
Eli Sagy <sagyeli@gmail.com>
2015-12-16 02:12:40 +0200

    Some design improvements for Details form and some design bugs fixes

--- app/src/main/java/com/themealz/themealz/DetailsForm.java
+++ app/src/main/java/com/themealz/themealz/DetailsForm.java
@@ -1,7 +1,12 @@
 package com.themealz.themealz;
 
 import android.app.Activity;
+import android.content.Context;
+import android.graphics.Typeface;
 import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
 
 public class DetailsForm extends Activity {
 
@@ -9,5 +14,22 @@ public class DetailsForm extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_details_form);
+
+        overrideFonts(this, findViewById(android.R.id.content));
+    }
+
+    private void overrideFonts(final Context context, final View v) {
+        try {
+            if (v instanceof ViewGroup) {
+                ViewGroup vg = (ViewGroup) v;
+                for (int i = 0; i < vg.getChildCount(); i++) {
+                    View child = vg.getChildAt(i);
+                    overrideFonts(context, child);
+                }
+            } else if (v instanceof TextView) {
+                ((TextView) v).setTypeface(Typeface.createFromAsset(context.getAssets(), "fonts/regular.ttf"));
+            }
+        } catch (Exception e) {
+        }
     }
 }
